c7577e558f8e63ccac0703f9d2ce6144e592c11f
shubhamjain0797 <42229987+shubhamjain0797@users.noreply.github.com>
2019-10-09 09:36:39 +0530

    Defining LCM & HCF functions

--- Math_functions.py
+++ Math_functions.py
@@ -9,3 +9,16 @@ def extracr_numbers_from_text(text):
             pass
     return(l)
 
+def lcm(a,b):
+    L=a if a>b else b
+    while L<=a*b:
+        if L%a==0 and L%b==0:
+            return L
+        L+=1
+
+def hcf(a,b):
+    H=a if a<b else b
+    while H>=1:
+        if a%H==0 and b%H==0:
+            return H
+        H-=1
